5114f2f96efe75adb7c0a2eafc2bbb6d17814188
deana <dvandegriff1@gmail.com>
2022-05-06 13:57:52 -0500

    adds and tests signup page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,24 @@
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to myGPA Calculator",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
 <template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<!-- <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -15,4 +35,4 @@ export default {
     HelloWorld,
   },
 };
-</script>
+</script> -->
