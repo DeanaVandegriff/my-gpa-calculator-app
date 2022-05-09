1e33123f25f47cac400bc3f5b40ab75264dd699d
deana <dvandegriff1@gmail.com>
2022-05-09 14:27:05 -0500

    adds table to index page

--- src/views/CoursesIndex.vue
+++ src/views/CoursesIndex.vue
@@ -19,9 +19,30 @@ export default {
 <template>
   <div class="index">
     <h1>Zach Morris' Courses</h1>
-    <div v-for="course in courses" v-bind:key="course.id">
+
+    <!-- <div v-for="course in courses" v-bind:key="course.id">
       <h2>Course: {{ course.name }}</h2>
       <h2>Grade: {{ course.grade }}</h2>
-    </div>
+    </div> -->
+    <table class="table table-bordered">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col" sortable="semester_taken">Semester</th>
+          <th scope="col">Course Name</th>
+          <th scope="col">Grade</th>
+          <th scope="col">Credits</th>
+          <th scope="col">Level</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="course in courses" v-bind:key="course.id">
+          <td>{{ course.semester_taken }}</td>
+          <td>{{ course.name }}</td>
+          <td>{{ course.grade }}</td>
+          <td>{{ course.credits }}</td>
+          <td>{{ course.level }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
