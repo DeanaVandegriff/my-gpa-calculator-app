a2362296328e0104e2c7e9227d297b9d06798d2a
deana <dvandegriff1@gmail.com>
2022-05-16 14:20:01 -0500

    adds bootstrap table and implements column sort

--- src/views/CoursesIndex.vue
+++ src/views/CoursesIndex.vue
@@ -18,8 +18,10 @@ export default {
   },
   methods: {
     calcGPA() {
-      var total = this.courses.map(({ point_value }) => point_value).reduce((a, b) => a + b, 0);
-      this.GPA = Math.round((total / this.courses.length) * 100) / 100;
+      var pointTotal = this.courses.map(({ point_value }) => point_value).reduce((a, b) => a + b, 0);
+      var creditTotal = this.courses.map(({ credits }) => credits).reduce((a, b) => a + b, 0);
+      // this.GPA = Math.round((pointTotal / creditTotal) * 100) / 100;
+      this.GPA = (pointTotal / creditTotal).toFixed(2);
     },
   },
 };
@@ -31,20 +33,24 @@ export default {
     <button v-on:click="calcGPA()">Cumulative GPA:</button>
     <h2>{{ GPA }}</h2>
     <div class="container">
-      <table class="table table-bordered">
+      <table class="table table-bordered" data-toggle="table">
         <thead class="thead-light">
           <tr>
+            <th data-sortable="true" scope="semester_taken">Semester</th>
             <th scope="col">Course Name</th>
             <th scope="col">Grade</th>
-            <th scope="col">Point Value</th>
+            <!-- <th scope="col">Credits</th>
+            <th scope="col">Point Value</th> -->
             <th scope="col">More Info</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="course in courses" v-bind:key="course.id">
+            <td>{{ course.semester_taken }}</td>
             <td>{{ course.name }}</td>
             <td>{{ course.grade }}</td>
-            <td>{{ course.point_value }}</td>
+            <!-- <td>{{ course.credits }}</td>
+            <td>{{ course.point_value }}</td> -->
             <td><a v-bind:href="`/courses/${course.id}/edit`">More Info!</a></td>
           </tr>
         </tbody>
