5c9ae7803f6cd47610125ac79487aad8ce9e12b1
deana <dvandegriff1@gmail.com>
2022-05-18 15:45:09 -0500

    stored user name in localstorage and then rendered on index page

--- src/views/CoursesIndex.vue
+++ src/views/CoursesIndex.vue
@@ -8,6 +8,7 @@ export default {
       point_value: [],
       total: "",
       GPA: "",
+      name: "",
     };
   },
   created: function () {
@@ -15,13 +16,17 @@ export default {
       this.courses = response.data;
       console.log("Courses", this.courses);
     });
+    this.getUserName();
   },
   methods: {
     calcGPA() {
       var pointTotal = this.courses.map(({ point_value }) => point_value).reduce((a, b) => a + b, 0);
       var creditTotal = this.courses.map(({ credits }) => credits).reduce((a, b) => a + b, 0);
-      // this.GPA = Math.round((pointTotal / creditTotal) * 100) / 100;
       this.GPA = (pointTotal / creditTotal).toFixed(2);
+    },
+    getUserName() {
+      const userName = localStorage.getItem("name");
+      this.name = userName;
     },
   },
 };
@@ -29,19 +34,17 @@ export default {
 
 <template>
   <div class="index">
-    <h1>Courses</h1>
+    <h1>{{ this.name }}'s Courses</h1>
     <button v-on:click="calcGPA()">Cumulative GPA:</button>
     <h2>{{ GPA }}</h2>
     <div class="container">
-      <table class="table table-bordered table-striped" id="sortTable">
+      <table class="table table-bordered table-striped">
         <thead class="thead-light">
           <tr>
-            <th scope="semester_taken" data-sortable="true">Semester</th>
-            <th scope="col" data-sortable="true">Course Name</th>
-            <th scope="col" data-sortable="true">Grade</th>
-            <!-- <th scope="col">Credits</th>
-            <th scope="col">Point Value</th> -->
-            <th scope="col" data-sortable="true">More Info</th>
+            <th scope="semester_taken">Semester</th>
+            <th scope="col">Course Name</th>
+            <th scope="col">Grade</th>
+            <th scope="col">More Info</th>
           </tr>
         </thead>
         <tbody>
@@ -49,8 +52,6 @@ export default {
             <td>{{ course.semester_taken }}</td>
             <td>{{ course.name }}</td>
             <td>{{ course.grade }}</td>
-            <!-- <td>{{ course.credits }}</td>
-            <td>{{ course.point_value }}</td> -->
             <td><a v-bind:href="`/courses/${course.id}/edit`">More Info!</a></td>
           </tr>
         </tbody>
@@ -58,6 +59,7 @@ export default {
     </div>
   </div>
 </template>
+
 <style>
 .table-striped > tbody > tr:nth-child(odd) > td,
 .table-striped > tbody > tr:nth-child(odd) > th {
